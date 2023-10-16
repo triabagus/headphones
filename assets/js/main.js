74f39ed284df12860cb01a1b23d050e79f53ec2b
triabagus <triatop9@gmail.com>
2023-10-16 16:20:48 +0700

    favorite section style

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,18 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 
 /*=============== SWIPER FAVORITES ===============*/ 
+const swiper = new Swiper('.favorite__swiper', {
+  loop: true,
+  slidesPerView: 'auto',
+  centeredSlides: 'auto',
+  grabCursor: true,
 
+  breakpoints: {
+    768:{
+      slidesPerView: 3,
+    }
+  }
+})
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
