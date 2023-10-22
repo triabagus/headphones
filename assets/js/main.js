e946a8410e0abba993cc89059742ae3860998c6a
triabagus <triatop9@gmail.com>
2023-10-22 22:44:53 +0700

    Scroll bar, scroll up

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,7 +48,14 @@ const swiper = new Swiper('.favorite__swiper', {
 })
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
 
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
