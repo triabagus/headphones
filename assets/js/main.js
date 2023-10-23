25707a54ef8ba0502df719fd67d1182a00af9e05
triabagus <triatop9@gmail.com>
2023-10-23 21:15:14 +0700

    header: scroll blur

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,14 @@ const linkAction = () => {
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== ADD BLUR HEADER ===============*/
-
+const blurHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
+};
+window.addEventListener("scroll", blurHeader);
 
 /*=============== SWIPER FAVORITES ===============*/ 
 const swiper = new Swiper('.favorite__swiper', {
